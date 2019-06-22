89badd685cbc3edc1663ffa8259d391816b4e6ac
ww2kvn <ww2kvn@ww2kvns-MBP.home>
2019-06-22 19:46:20 -0400

    1.pulled all images and refactored code and create image artical function

--- js/js.js
+++ js/js.js
@@ -42,26 +42,61 @@
 		"mobile_img" : "set01_05_mobile.jpg"
 	}
 
-	]	
+	]
 
-	for(var i = 0; i < adData.length; i++){
-		var deskImg = adData[i]['desk_img'];
-		var createArt = document.createElement('article');
-		artTag.appendChild(createArt)
-		var getArt = document.querySelectorAll("article");
-		artTag.style.backgroundImage="url(images/"+deskImg+")";
+	var artTag = document.querySelector('.ad-container');
+	var browserWidth = window.innerWidth;
 
-		for(var j = 0; j < getArt.length; j++){
-			var artName = getArt[j];
-			var classAdded = getArt[j].className="ad-slide";
-			//var addBG = getArt[j].style.backgroundImage = "url(images/"+deskImg+")";
+	function imageArtData(){
+
+		for(var i = 0 ; i < adData.length ; i++){
+
+			if( browserWidth <= 1020){
+				deskImg = adData[i]['mobile_img'];
+				console.log("Mobile size");
+			}else{
+				deskImg = adData[i]['desk_img'];
+				console.log("Desktop");
+			}
+
+			console.log(browserWidth);
+
+				
+			var createArt = document.createElement('article');
+			artTag.appendChild(createArt);
+			var getArt = document.querySelectorAll("article");
+			artTag.style.backgroundImage="url(images/"+deskImg+")";
+			//art
+
+			for(var j = 0; j < getArt.length; j++){
+				var artName = getArt[j];
+				var classAdded = getArt[j].className="ad-slide";
+				//var addBG = getArt[j].style.backgroundImage = "url(images/"+deskImg+")";
+			}
+			
+			artName.style.backgroundImage = "url(images/"+deskImg+")";
+			artName.style.backgroundSize = "cover";
+
 		}
-		
-		artName.style.backgroundImage = "url(images/"+deskImg+")";
-		artName.style.backgroundSize = "cover";
+		console.log(adData);
+		console.log(deskImg);
 
 	}
-	console.log(adData);
-	console.log(deskImg);
+
+	
+	/*var browserResize = window.addEventListener("resize", function(){
+
+		if( browserWidth <= 1020){
+			console.log("Mobile size");
+		}else{
+			console.log("Desktop");
+		}
+
+		console.log(browserWidth);
+		
+	});*/
+
+
+	imageArtData();
 
 })();

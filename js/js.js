5deb0da800743a8500eb3f9a35c2f67c81668e65
ww2kvn <ww2kvn@ww2kvns-MBP.home>
2019-06-23 21:31:26 -0400

    1.created the arrow onclick event feature. Checked and tracked number of clicks to cycle through article slides

--- js/js.js
+++ js/js.js
@@ -45,11 +45,15 @@
 	]
 
 	var artTag = document.querySelector('.ad-container');
+	var highLight = document.querySelector('.ad-highlight');
+	var rightArrow = document.querySelector('#arrow-right');
+	var leftArrow = document.querySelector('#arrow-left');
+	var hiddenArrow = document.querySelector('hide-arrow');
 	var browserWidth = window.innerWidth;
 
 	function imageArtData(){
 
-		for(var i = 0 ; i < adData.length ; i++){
+		for(let i = 0 ; i < adData.length ; i++){
 
 			if( browserWidth <= 1020){
 				deskImg = adData[i]['mobile_img'];
@@ -58,34 +62,95 @@
 				deskImg = adData[i]['desk_img'];
 				console.log("Desktop");
 			}
-
-			console.log(browserWidth);
-
-				
-			var createArt = document.createElement('article');
+	
+			let createArt = document.createElement('article');
 			artTag.appendChild(createArt);
 			var getArt = document.querySelectorAll("article");
-			artTag.style.backgroundImage="url(images/"+deskImg+")";
+			//artTag.style.backgroundImage="url(images/"+deskImg+")";
 			//art
 
-			for(var j = 0; j < getArt.length; j++){
+			for(let j = 0; j < getArt.length; j++){
 				var artName = getArt[j];
-				var classAdded = getArt[j].className="ad-slide";
-				//var addBG = getArt[j].style.backgroundImage = "url(images/"+deskImg+")";
+				let classAdded = getArt[j].className="ad-slide";
 			}
 			
 			artName.style.backgroundImage = "url(images/"+deskImg+")";
 			artName.style.backgroundSize = "cover";
 
 		}
+
 		console.log(adData);
-		console.log(deskImg);
+		console.log(getArt[1]);
+		
 
 	}
+
+	var count = 0; 
+	console.log(count);
+
+	function arrowClick(){
+		rightArrow.addEventListener("click",function(){
+			//alert('you clicked left Arrow');
+			//console.log(adData.length);
+
+			if(leftArrow.classList = 'hide-arrow' ){
+				leftArrow.classList.remove('hide-arrow');
+				leftArrow.classList.add('click-able');
+				highLight.classList.add('stop-point');
+			}
+
+			if(count < adData.length ){
+				count += 1; 
+			}  else{
+				console.log('Youve reach the end ');
+				rightArrow.className= "hide-arrow";
+			}
+			
+			console.log(count);
+			
+			console.log(adData.length);
+
+			/*for(let i = 0 ; i < adData.length; i++ ){
+				console.log(i++);	
+			}*/
+		});
+
+
+		leftArrow.addEventListener("click",function(){
+			//alert('you clicked left Arrow');
+			//console.log(adData.length);
+
+				if(rightArrow.classList = 'hide-arrow' ){
+					rightArrow.classList.remove('hide-arrow');
+					rightArrow.classList.add('click-able');
+				}
+
+				if(count > 1){
+					count -= 1; 
+				}else{
+					leftArrow.className= "hide-arrow";
+					highLight.classList.remove('stop-point');
+					console.log('Youve reached the beginning ');
+				}
+			
+			console.log(count);
+			
+			//console.log(adData.length);
+
+			/*for(let i = 0 ; i < adData.length; i++ ){
+				console.log(i++);	
+			}*/
+		});
+	}
+
+	arrowClick();
+
+
 
 	
 	/*var browserResize = window.addEventListener("resize", function(){
 
+		var browserWidth = window.innerWidth;
 		if( browserWidth <= 1020){
 			console.log("Mobile size");
 		}else{
